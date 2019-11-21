43e67fc76a00165c0cbc8d02bf42c0a7455d88b6
Brian Fiszman <fiszmanbrian@gmail.com>
2019-11-21 03:13:16 +0000

    Feature/month grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import MonthGrid from './components/MonthGrid';
+import { AppContainer, Header, Logo } from './App.styles';
 
 const App: React.FC = () => {
+  console.log(typeof MonthGrid);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <Header>
+        <MonthGrid />
+      </Header>
+    </AppContainer>
   );
-}
+};
 
 export default App;
